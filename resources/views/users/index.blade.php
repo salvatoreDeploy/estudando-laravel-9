cf8e3c7ba254b0aa683bb84202bdf5b8f6806c9a
salvatoreDeploy <liderhenrique@gmail.com>
2023-08-02 23:25:01 -0300

    Aula-10-Registros Banco de Dados

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,9 @@
-Listagem de Usuarios
+<h1>Listagem de Usuarios</h1>
+
+<ul>
+    @foreach($users as $user)
+        <li>
+            {{$user->name}} - {{$user->email}}
+        </li>
+    @endforeach
+</ul>
