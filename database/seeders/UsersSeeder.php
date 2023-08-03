6379827a08c8d0852eb15b983d26ab5e99766417
salvatoreDeploy <liderhenrique@gmail.com>
2023-08-02 23:05:25 -0300

    Aula-09-Trabalhar com Seeders

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Joe Doe',
+            'email' => 'joedoe@email.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
