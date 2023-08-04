34b0140b26162b0f65cd7a2eb3297c292a813457
salvatoreDeploy <liderhenrique@gmail.com>
2023-08-04 20:48:17 -0300

    Aula-12-Template Blade

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,15 @@
-<h1>Listagem de Usuarios</h1>
+@extends('layouts.master')
 
-<ul>
-    @foreach($users as $user)
-        <li>
-            {{$user->name}} - {{$user->email}} | <a href="{{ route('users.show', ['id' => $user->id])}}">Detalhe</a>
-        </li>
-    @endforeach
-</ul>
+@section('title', 'Listagem de Usuario')
+
+@section('content')
+    <h1>Listagem de Usuarios</h1>
+
+    <ul>
+        @foreach($users as $user)
+            <li>
+                {{$user->name}} - {{$user->email}} | <a href="{{ route('users.show', ['id' => $user->id])}}">Detalhe</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,13 @@
-<h1>Detalhe do Usuario {{ $user->name }}</h1>
+@extends('layouts.master')
 
-<ul>
-    <li>{{ $user->name }}</li>
-    <li>{{ $user->email }}</li>
-    <li>{{ $user->created_at }}</li>
-</ul>
+@section('title', 'Detalhes do Usuario')
+
+@section('content')
+    <h1>Detalhe do Usuario {{ $user->name }}</h1>
+
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+        <li>{{ $user->created_at }}</li>
+    </ul>
+@endsection
